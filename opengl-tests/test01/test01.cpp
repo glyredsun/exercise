0447f90d429ed8d52a7c4ad3c6f61f1e4d655ad2
gly <glyredsun@163.com>
2016-01-29 17:30:59 +0800

    changed CMakeLists.txt

--- opengl-tests/test01/test01.cpp
+++ opengl-tests/test01/test01.cpp
@@ -22,7 +22,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 	{
 	case GLFW_KEY_ESCAPE:
 	{
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
+		glfwSetWindowShouldClose(window, GL_TRUE);
 		break;
 	}
 	default:
